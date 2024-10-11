8c4da2cab4944174307bc21a99a426452294b0f0
Fetisov <rofetisov@mail.ru>
2024-10-11 14:07:20 +0300

    chore: init commit

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Layout from "./shared/ui/Layout.tsx";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {routerPaths} from "./app/router.tsx";
+
+const router = createBrowserRouter(routerPaths)
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+      <QueryClientProvider client={queryClient}>
+          <Layout>
+              <RouterProvider router={router} />
+          </Layout>
+      </QueryClientProvider>
+  </StrictMode>,
+)
