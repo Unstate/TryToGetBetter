ac6fd254f7f378916f98bcb327d33038eeb13ac9
Fetisov <rofetisov@mail.ru>
2024-10-11 18:10:50 +0300

    test commit

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -1,33 +1,45 @@
-import Form from "@/shared/widjets/Form.tsx";
-import Nav from "@/shared/ui/Nav.tsx";
-import ModalCreateOrganization from "@/shared/widjets/organizations/ModalCreateOrganization.tsx";
-import {getOrganizations} from "@/shared/api/organization.ts";
-import Loading from "@/shared/ui/Loading.tsx";
-import ErrorBoundary from "@/shared/ui/ErrorBoundary.tsx";
+import Form from '@/shared/widjets/Form.tsx';
+import Nav from '@/shared/ui/Nav.tsx';
+import ModalCreateOrganization from '@/shared/widjets/organizations/ModalCreateOrganization.tsx';
+import { getOrganizations } from '@/shared/api/organization.ts';
+import Loading from '@/shared/ui/Loading.tsx';
+import ErrorBoundary from '@/shared/ui/ErrorBoundary.tsx';
 
-import {CREATE_USER_FORM_FIELDS} from "@/features/constants/CreateUserForm.ts";
+import { CREATE_USER_FORM_FIELDS } from '@/features/constants/CreateUserForm.ts';
 
-import {useQuery} from "@tanstack/react-query";
+import { useQuery } from '@tanstack/react-query';
 
 const HomePage = () => {
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['organization'],
+    queryFn: () => getOrganizations(),
+  });
 
-    const {data, isLoading, isError, error} = useQuery({queryKey:['organization'], queryFn: () => getOrganizations()})
+  if (isLoading) {
+    return <Loading />;
+  }
 
-    if (isLoading) {
-        return <Loading />
-    }
+  if (isError) {
+    return <ErrorBoundary message={error.message} />;
+  }
 
-    if (isError) {
-        return <ErrorBoundary message={error.message} />
-    }
+  return (
+    <section
+      className={
+        'w-full min-h-screen flex flex-col justify-center items-center gap-2'
+      }
+    >
+      <Nav />
+      {!!data?.length && (
+        <Form
+          fields={CREATE_USER_FORM_FIELDS}
+          title={'Create a new User'}
+          organizations={data}
+        />
+      )}
+      <ModalCreateOrganization />a
+    </section>
+  );
+};
 
-    return (
-        <section className={'w-full min-h-screen flex flex-col justify-center items-center gap-2'}>
-            <Nav />
-            {!!data?.length && <Form fields={CREATE_USER_FORM_FIELDS} title={"Create a new User"} organizations={data}/>}
-            <ModalCreateOrganization />
-        </section>
-    )
-}
-
-export default HomePage
+export default HomePage;
