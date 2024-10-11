9c3899bf078fb5e0d2cb064204e3094dc7e1640d
Fetisov <rofetisov@mail.ru>
2024-10-11 14:13:31 +0300

    feat: add constants

---
+++ src/features/constants/CreateUserForm.ts
@@ -0,0 +1,17 @@
+import {TField} from "../../shared/widjets/Form.tsx";
+
+export const CREATE_USER_FORM_FIELDS:TField[] = [
+    {
+        name: 'name',
+        label: 'Enter name of user',
+        placeholder: 'Unstate',
+    },{
+        name: 'email',
+        label: 'Enter user`s email',
+        placeholder: 'Someone@gmail.com',
+    },{
+        name: 'mobile',
+        label: 'Enter user`s mobile number',
+        placeholder: '89106925670 or +79106925670',
+    },
+]
